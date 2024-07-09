863143d981477c8a767ce17a4ed2cc17a9e34248
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-07-09 15:30:47 +0500

    Seventh commit

--- src/components/dashboard/EditProfile.jsx
+++ src/components/dashboard/EditProfile.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EditProfile = ({ onClose }) => {
+  const [lengthUnit, setLengthUnit] = useState("cm");
+  const [weightUnit, setWeightUnit] = useState("kg");
+  const [selectedGoal, setSelectedGoal] = useState("");
+  const [selectedMeal, setSelectedMeal] = useState("");
+
+  const handleLengthUnitChange = (selectedUnit) => {
+    setLengthUnit(selectedUnit);
+  };
+
+  const handleWeightUnitChange = (selectedUnit) => {
+    setWeightUnit(selectedUnit);
+  };
+
+  const handleGoalChange = (goal) => {
+    setSelectedGoal(goal);
+  };
+
+  const handleMealChange = (meal) => {
+    setSelectedMeal(meal);
+  };
+
   return (
     <div className="fixed top-0 right-0 h-full overflow-y-auto max-w-md bg-darkgray rounded-t-3xl rounded-b-3xl z-50 p-5">
       <div className="w-full px-5 pb-5">
@@ -34,66 +55,173 @@ const EditProfile = ({ onClose }) => {
               placeholder="Age"
               className="mb-3 w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
             />
-            <div className="my-7 flex w-full items-center gap-2 rounded-2xl ">
-              <input
-                required
-                name="feet"
-                type="number"
-                placeholder="Height"
-                className="mb-3 w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
-              />
-              <input
-                required
-                name="inches"
-                type="number"
-                placeholder="Height"
-                className="mb-3 w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
-              />
-              <div className="mr-1 flex h-12 w-32 items-center mb-[14px] rounded-3xl bg-primary">
-                <div className="ml-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl bg-primary text-[#1E2534]">
+
+            <div className="my-7 flex w-full h-12 items-center justify-center gap-2 rounded-3xl bg-primary p-4">
+              {lengthUnit === "ft" && (
+                <input
+                  required
+                  name="cm"
+                  type="number"
+                  placeholder="Height (cm)"
+                  className="w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
+                />
+              )}
+              {lengthUnit === "cm" && (
+                <>
+                  <input
+                    required
+                    name="feet"
+                    type="number"
+                    placeholder="Feet"
+                    className="w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
+                  />
+                  <input
+                    required
+                    name="inches"
+                    type="number"
+                    placeholder="Inches"
+                    className="w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
+                  />
+                </>
+              )}
+
+              <div className="flex gap-2 ml-4">
+                <div
+                  onClick={() => handleLengthUnitChange("ft")}
+                  className={`flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl ${
+                    lengthUnit === "cm"
+                      ? "bg-primary text-darkbrown"
+                      : "bg-[#1E2534] text-primary"
+                  }`}
+                >
                   Cm
                 </div>
-                <div className="mr-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl bg-[#1E2534] text-white">
+                <div
+                  onClick={() => handleLengthUnitChange("cm")}
+                  className={`flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl ${
+                    lengthUnit === "ft"
+                      ? "bg-primary text-darkbrown"
+                      : "bg-[#1E2534] text-primary"
+                  }`}
+                >
                   Ft
                 </div>
               </div>
             </div>
-            <div className="my-7 flex w-full items-center gap-2 rounded-2xl bg-white">
-              <input
-                required
-                name="weight"
-                type="number"
-                placeholder="Weight"
-                className="mb-3 w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
-              />
-              <div className="mr-1 flex h-12 w-32 items-center mb-[14px] rounded-3xl bg-primary">
-                <div className="ml-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl bg-[#1E2534] text-white">
-                  kg
+            <div className="my-7 flex w-full h-12 justify-center items-center gap-2 rounded-3xl bg-primary">
+              {weightUnit === "lbs" && (
+                <>
+                  <input
+                    required
+                    name="weightKg"
+                    type="number"
+                    placeholder="Weight (kg)"
+                    className=" w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
+                  />
+                </>
+              )}
+              {weightUnit === "kg" && (
+                <>
+                  <input
+                    required
+                    name="weightLbs"
+                    type="number"
+                    placeholder="Weight (lbs)"
+                    className=" w-full rounded-3xl px-6 py-4 text-sm font-medium leading-4 outline-black"
+                  />
+                </>
+              )}
+
+              <div className="mr-1 flex h-12 w-32 items-center rounded-3xl bg-primary">
+                <div
+                  onClick={() => handleWeightUnitChange("lbs")}
+                  className={`ml-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-3xl ${
+                    weightUnit === "kg"
+                      ? "bg-primary text-darkbrown"
+                      : "bg-[#1E2534] text-primary"
+                  }`}
+                >
+                  Kg
                 </div>
-                <div className="mr-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-2xl bg-primary text-[#1E2534]">
+                <div
+                  onClick={() => handleWeightUnitChange("kg")}
+                  className={`mr-1 flex h-9 w-16 cursor-pointer items-center justify-center rounded-3xl ${
+                    weightUnit === "lbs"
+                      ? "bg-primary text-darkbrown"
+                      : "bg-[#1E2534] text-primary"
+                  }`}
+                >
                   Lbs
                 </div>
               </div>
             </div>
             <div>
-              <p className="font-semibold text-darkbrown font-semi mb-4 text-2xl">
+              <div className="font-semibold text-darkbrown font-semi mb-4 text-2xl">
                 Goal
-              </p>
-              <label className="mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl bg-primary px-5 py-4">
+              </div>
+              <label
+                className={`mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl ${
+                  selectedGoal === "lose"
+                    ? "bg-primary text-white"
+                    : "bg-primary"
+                } px-5 py-4`}
+                onClick={() => handleGoalChange("lose")}
+              >
                 <span>Lose Weight</span>
-                <div className="h-4 w-4 rounded-full border-2 border-caption"></div>
+                <div
+                  className={`h-4 w-4 rounded-full border-2 border-lightbrown ${
+                    selectedGoal === "lose" ? "bg-lightgreen border-none" : ""
+                  }`}
+                ></div>
               </label>
-              <label className="mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl bg-primary px-5 py-4">
+              <label
+                className={`mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl ${
+                  selectedGoal === "maintain"
+                    ? "bg-primary text-white"
+                    : "bg-primary"
+                } px-5 py-4`}
+                onClick={() => handleGoalChange("maintain")}
+              >
                 <span>Maintain Weight</span>
-                <div className="h-4 w-4 rounded-full border-2 border-caption"></div>
+                <div
+                  className={`h-4 w-4 rounded-full border-2 border-lightbrown ${
+                    selectedGoal === "maintain"
+                      ? "bg-lightgreen border-none"
+                      : ""
+                  }`}
+                ></div>
               </label>
-              <label className="mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl bg-primary px-5 py-4">
+              <label
+                className={`mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl ${
+                  selectedGoal === "gain"
+                    ? "bg-primary text-white"
+                    : "bg-primary"
+                } px-5 py-4`}
+                onClick={() => handleGoalChange("gain")}
+              >
                 <span>Gain Weight</span>
-                <div className="h-4 w-4 rounded-full border-2 border-caption"></div>
+                <div
+                  className={`h-4 w-4 rounded-full border-2 border-lightbrown ${
+                    selectedGoal === "gain" ? "bg-lightgreen border-none" : ""
+                  }`}
+                ></div>
               </label>
-              <label className="mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl bg-primary px-5 py-4">
+              <label
+                className={`mb-4 flex h-12 cursor-pointer items-center justify-between rounded-3xl ${
+                  selectedGoal === "explore"
+                    ? "bg-primary text-white"
+                    : "bg-primary"
+                } px-5 py-4`}
+                onClick={() => handleGoalChange("explore")}
+              >
                 <span>Just Exploring</span>
-                <div className="h-4 w-4 rounded-full border-2 border-caption"></div>
+                <div
+                  className={`h-4 w-4 rounded-full border-2 border-lightbrown ${
+                    selectedGoal === "explore"
+                      ? "bg-lightgreen border-none"
+                      : ""
+                  }`}
+                ></div>
               </label>
             </div>
             <div>
@@ -101,16 +229,44 @@ const EditProfile = ({ onClose }) => {
                 Daily Meal Amount
               </p>
               <div className="mt-4 flex items-center justify-center gap-4">
-                <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold  bg-primary">
+                <div
+                  className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold ${
+                    selectedMeal === "3"
+                      ? "bg-[#1E2534] text-primary"
+                      : "bg-primary"
+                  }`}
+                  onClick={() => handleMealChange("3")}
+                >
                   3
                 </div>
-                <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold bg-primary ">
+                <div
+                  className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold ${
+                    selectedMeal === "4"
+                      ? "bg-[#1E2534] text-primary"
+                      : "bg-primary"
+                  }`}
+                  onClick={() => handleMealChange("4")}
+                >
                   4
                 </div>
-                <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold bg-primary ">
+                <div
+                  className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold ${
+                    selectedMeal === "5"
+                      ? "bg-[#1E2534] text-primary"
+                      : "bg-primary"
+                  }`}
+                  onClick={() => handleMealChange("5")}
+                >
                   5
                 </div>
-                <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold bg-primary ">
+                <div
+                  className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-xs font-semibold ${
+                    selectedMeal === "6"
+                      ? "bg-[#1E2534] text-primary"
+                      : "bg-primary"
+                  }`}
+                  onClick={() => handleMealChange("6")}
+                >
                   6
                 </div>
               </div>
